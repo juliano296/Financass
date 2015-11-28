f30b4868a157199afd8b77270727eeac318b950f
Juliano <julianomdelima@aol.com>
2015-11-28 00:02:53 -0200

    esse e um site de financas

---
+++ Financaas/Financaas/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financaas.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            ViewBag.Message = "Este é um site de financas";
+            return View();
+
+        }
+
+    }
+}
